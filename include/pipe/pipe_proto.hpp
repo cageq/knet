d58b058f3f43067d006ba4b296d4e26b39341aff
arthur <fatih@qq.com>
2021-02-25 17:14:26 +0800

    update

--- include/pipe/pipe_proto.hpp
+++ include/pipe/pipe_proto.hpp
@@ -6,11 +6,15 @@
 namespace knet{ 
 
 	namespace pipe{ 
-		enum PipeMsgType { PIPE_MSG_SHAKE_HAND = 1, PIPE_MSG_HEART_BEAT, PIPE_MSG_DATA };
+		enum PipeMsgType {
+		 PIPE_MSG_SHAKE_HAND = 1, 
+		 PIPE_MSG_HEART_BEAT, 
+		 PIPE_MSG_DATA 
+		};
 
 		struct PipeMsgHead {
-			uint32_t length : 23;
-			uint32_t type : 9;
+			uint32_t length : 24;
+			uint32_t type : 8;
 
 			PipeMsgHead(uint16_t t = 0, uint32_t len = 0)
 				: length(len)
