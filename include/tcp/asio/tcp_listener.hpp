e73ff552fe2ad56af81dfa80447c8012e153728e
arthur <fatih@qq.com>
2021-07-28 13:36:48 +0800

    udpate

--- include/tcp/asio/tcp_listener.hpp
+++ include/tcp/asio/tcp_listener.hpp
@@ -19,294 +19,293 @@ namespace knet
 
 		using asio::ip::tcp;
 		template <class T, class Factory = KNetFactory<T>, class Worker = KNetWorker>
-		class TcpListener final : public KNetHandler<T>
+			class TcpListener final : public KNetHandler<T>
 		{
-		public:
-			using TPtr = std::shared_ptr<T>;
-			using FactoryPtr = Factory *;
-			using WorkerPtr = std::shared_ptr<Worker>;
-			using SocketPtr = std::shared_ptr<TcpSocket<T>>;
+			public:
+				using TPtr = std::shared_ptr<T>;
+				using FactoryPtr = Factory *;
+				using WorkerPtr = std::shared_ptr<Worker>;
+				using SocketPtr = std::shared_ptr<TcpSocket<T>>;
 
-			TcpListener(
-				FactoryPtr fac = nullptr, WorkerPtr lisWorker = nullptr)
-			{
-				if (lisWorker == nullptr){
-					m.listen_worker = std::make_shared<Worker>(); 
-					m.listen_worker->start();
-				}else {
-					m.listen_worker = lisWorker;
-				}
-				
-				m.factory = fac;
-				if (fac != nullptr)
-				{					
-					add_factory_event_handler(std::is_base_of<KNetHandler<T>, Factory>(), fac);
-				}
-				 
-				tcp_acceptor = std::make_shared<asio::ip::tcp::acceptor>(m.listen_worker->context()); 
-			}
-
-			TcpListener(WorkerPtr lisWorker)
-			{
-
-				if (lisWorker == nullptr){
-					m.listen_worker = std::make_shared<Worker>(); 
-					m.listen_worker->start();
-				}else {
-					m.listen_worker = lisWorker;
-				}
- 
-				m.factory = nullptr; 
-				tcp_acceptor = std::make_shared<asio::ip::tcp::acceptor>(m.listen_worker->context()); 
-			}
-
-			void add_worker(WorkerPtr worker)
-			{
-				if (worker)
+				TcpListener(FactoryPtr fac = nullptr, WorkerPtr lisWorker = nullptr)
 				{
-					m.user_workers.push_back(worker);
-				}
-			}
-
-			TcpListener(const TcpListener &) = delete;
-
-			~TcpListener() {}
-
-			bool start(NetOptions opt, void *sslCtx = nullptr)
-			{
-				m.options = opt;
-				m.ssl_context = sslCtx;
-				if (!m.is_running)
-				{
-					m.is_running = true;
-					asio::ip::tcp::endpoint endpoint(asio::ip::make_address(opt.host), opt.port);
-
-					// this->tcp_acceptor.open(asio::ip::tcp::v4());
-					this->tcp_acceptor->open(endpoint.protocol());
-					if (tcp_acceptor->is_open())
-					{
-						this->tcp_acceptor->set_option(asio::socket_base::reuse_address(true));
-						//	this->tcp_acceptor.set_option(asio::ip::tcp::no_delay(true));
-						this->tcp_acceptor->non_blocking(true);
-
-						asio::socket_base::send_buffer_size SNDBUF(m.options.send_buffer_size);
-						this->tcp_acceptor->set_option(SNDBUF);
-						asio::socket_base::receive_buffer_size RCVBUF(m.options.recv_buffer_size);
-						this->tcp_acceptor->set_option(RCVBUF);
-
-						asio::error_code ec;
-						this->tcp_acceptor->bind(endpoint, ec);
-						if (ec)
-						{
-							elog("bind address failed {}:{}", opt.host, opt.port);
-							m.is_running = false;
-							return false;
-						}
-						this->tcp_acceptor->listen(m.options.backlogs, ec);
-
-						if (ec)
-						{
-							elog("start listen failed");
-							m.is_running = false;
-							return false;
-						}
-						this->do_accept();
+					if (lisWorker == nullptr){
+						m.listen_worker = std::make_shared<Worker>(); 
+						m.listen_worker->start();
+					}else {
+						m.listen_worker = lisWorker;
 					}
-					else
+
+					m.factory = fac;
+					if (fac != nullptr)
+					{					
+						add_factory_event_handler(std::is_base_of<KNetHandler<T>, Factory>(), fac);
+					}
+
+					tcp_acceptor = std::make_shared<asio::ip::tcp::acceptor>(m.listen_worker->context()); 
+				}
+
+				TcpListener(WorkerPtr lisWorker)
+				{
+
+					if (lisWorker == nullptr){
+						m.listen_worker = std::make_shared<Worker>(); 
+						m.listen_worker->start();
+					}else {
+						m.listen_worker = lisWorker;
+					}
+
+					m.factory = nullptr; 
+					tcp_acceptor = std::make_shared<asio::ip::tcp::acceptor>(m.listen_worker->context()); 
+				}
+
+				void add_worker(WorkerPtr worker)
+				{
+					if (worker)
 					{
-						return false;
+						m.user_workers.push_back(worker);
 					}
 				}
-				return true;
-			}
 
-			bool start(uint16_t port = 9999, const std::string &host = "0.0.0.0", void *ssl = nullptr)
-			{
-				m.options.host = host;
-				m.options.port = port;
-				return start(m.options, ssl);
-			}
+				TcpListener(const TcpListener &) = delete;
 
-			void stop()
-			{
-				dlog("stop listener thread");
-				if (m.is_running)
+				~TcpListener() {}
+
+				bool start(NetOptions opt, void *sslCtx = nullptr)
 				{
-					m.is_running = false;
-					tcp_acceptor->close();
-					m.user_workers.clear();
-					m.listen_worker->stop();
+					m.options = opt;
+					m.ssl_context = sslCtx;
+					if (!m.is_running)
+					{
+						m.is_running = true;
+						asio::ip::tcp::endpoint endpoint(asio::ip::make_address(opt.host), opt.port);
+
+						// this->tcp_acceptor.open(asio::ip::tcp::v4());
+						this->tcp_acceptor->open(endpoint.protocol());
+						if (tcp_acceptor->is_open())
+						{
+							this->tcp_acceptor->set_option(asio::socket_base::reuse_address(true));
+							//	this->tcp_acceptor.set_option(asio::ip::tcp::no_delay(true));
+							this->tcp_acceptor->non_blocking(true);
+
+							asio::socket_base::send_buffer_size SNDBUF(m.options.send_buffer_size);
+							this->tcp_acceptor->set_option(SNDBUF);
+							asio::socket_base::receive_buffer_size RCVBUF(m.options.recv_buffer_size);
+							this->tcp_acceptor->set_option(RCVBUF);
+
+							asio::error_code ec;
+							this->tcp_acceptor->bind(endpoint, ec);
+							if (ec)
+							{
+								elog("bind address failed {}:{}", opt.host, opt.port);
+								m.is_running = false;
+								return false;
+							}
+							this->tcp_acceptor->listen(m.options.backlogs, ec);
+
+							if (ec)
+							{
+								elog("start listen failed");
+								m.is_running = false;
+								return false;
+							}
+							this->do_accept();
+						}
+						else
+						{
+							return false;
+						}
+					}
+					return true;
 				}
-			}
 
-			virtual bool handle_data(TPtr conn, const std::string &msg)
-			{
-				return invoke_data_chain(conn, msg);
-			}
-
-			virtual bool handle_event(TPtr conn, NetEvent evt)
-			{
-				bool ret = invoke_event_chain(conn, evt);
-				if (evt == EVT_RELEASE)
+				bool start(uint16_t port = 9999, const std::string &host = "0.0.0.0", void *ssl = nullptr)
 				{
-					this->release(conn);
+					m.options.host = host;
+					m.options.port = port;
+					return start(m.options, ssl);
 				}
-				return ret;
-			}
 
-			void add_event_handler(KNetHandler<T> *handler)
-			{
-				if (handler)
+				void stop()
 				{
-					m.event_handler_chain.push_back(handler);
+					dlog("stop listener thread");
+					if (m.is_running)
+					{
+						m.is_running = false;
+						tcp_acceptor->close();
+						m.user_workers.clear();
+						m.listen_worker->stop();
+					}
 				}
-			}
-			void add_first(KNetHandler<T> *handler){
-				if (handler)
-				{
-					auto beg = m.event_handler_chain.begin(); 
-					m.event_handler_chain.insert(beg, handler);
-				}
-			}
-			
-			void add_last(KNetHandler<T> *handler){
-				if (handler)
-				{
-					m.event_handler_chain.push_back(handler);
-				}
-			}
 
-		private:
-			inline void add_factory_event_handler(std::true_type, FactoryPtr fac)
-			{
-				auto evtHandler = dynamic_cast<KNetHandler<T> *>(fac);
-				if (evtHandler)
+				virtual bool handle_data(TPtr conn, const std::string &msg)
 				{
-					add_event_handler(evtHandler);
+					return invoke_data_chain(conn, msg);
 				}
-			}
 
-			inline void add_factory_event_handler(std::false_type, FactoryPtr fac)
-			{
-			}
+				virtual bool handle_event(TPtr conn, NetEvent evt)
+				{
+					bool ret = invoke_event_chain(conn, evt);
+					if (evt == EVT_RELEASE)
+					{
+						this->release(conn);
+					}
+					return ret;
+				}
 
-			bool invoke_data_chain(const TPtr &  conn, const std::string &msg)
-			{
-				bool ret = true;
-				for (auto &handler : m.event_handler_chain)
+				void add_event_handler(KNetHandler<T> *handler)
 				{
 					if (handler)
 					{
-						ret = handler->handle_data(conn, msg);
-						if (!ret)
-						{
-							break;
-						}
+						m.event_handler_chain.push_back(handler);
 					}
 				}
-				return ret;
-			}
-
-			bool invoke_event_chain(const TPtr &  conn, NetEvent evt)
-			{
-				bool ret = true;
-				for (auto &handler : m.event_handler_chain)
-				{
+				void add_first(KNetHandler<T> *handler){
 					if (handler)
 					{
-						ret = handler->handle_event(conn, evt);
-						if (!ret)
+						auto beg = m.event_handler_chain.begin(); 
+						m.event_handler_chain.insert(beg, handler);
+					}
+				}
+
+				void add_last(KNetHandler<T> *handler){
+					if (handler)
+					{
+						m.event_handler_chain.push_back(handler);
+					}
+				}
+
+			private:
+				inline void add_factory_event_handler(std::true_type, FactoryPtr fac)
+				{
+					auto evtHandler = dynamic_cast<KNetHandler<T> *>(fac);
+					if (evtHandler)
+					{
+						add_event_handler(evtHandler);
+					}
+				}
+
+				inline void add_factory_event_handler(std::false_type, FactoryPtr fac)
+				{
+				}
+
+				bool invoke_data_chain(const TPtr &  conn, const std::string &msg)
+				{
+					bool ret = true;
+					for (auto &handler : m.event_handler_chain)
+					{
+						if (handler)
 						{
-							break;
+							ret = handler->handle_data(conn, msg);
+							if (!ret)
+							{
+								break;
+							}
 						}
 					}
+					return ret;
 				}
-				return ret;
-			}
 
-			void release(const TPtr &  conn)
-			{
-				asio::post(m.listen_worker->context(), [this, conn]() {
-					if (m.factory)
-					{
-						m.factory->release(conn);
-					}
-				});
-			}
-			void do_accept()
-			{
-				// dlog("accept new connection ");
-				auto worker = this->get_worker();
-				if (!worker)
+				bool invoke_event_chain(const TPtr &  conn, NetEvent evt)
 				{
-					elog("no event worker, can't start");
-					return;
+					bool ret = true;
+					for (auto &handler : m.event_handler_chain)
+					{
+						if (handler)
+						{
+							ret = handler->handle_event(conn, evt);
+							if (!ret)
+							{
+								break;
+							}
+						}
+					}
+					return ret;
 				}
 
-				auto socket = std::make_shared<TcpSocket<T>>(worker->thread_id(), worker->context(), m.ssl_context);
-				tcp_acceptor->async_accept(socket->socket(), [this, socket, worker](std::error_code ec) {
-					if (!ec)
+				void release(const TPtr &  conn)
+				{
+					asio::post(m.listen_worker->context(), [this, conn]() {
+							if (m.factory)
+							{
+							m.factory->release(conn);
+							}
+							});
+				}
+				void do_accept()
+				{
+					// dlog("accept new connection ");
+					auto worker = this->get_worker();
+					if (!worker)
 					{
-						dlog("accept new connection ");
-						this->init_conn(worker, socket);
-						do_accept();
+						elog("no event worker, can't start");
+						return;
+					}
+
+					auto socket = std::make_shared<TcpSocket<T>>(worker->thread_id(), worker->context(), m.ssl_context);
+					tcp_acceptor->async_accept(socket->socket(), [this, socket, worker](std::error_code ec) {
+							if (!ec)
+							{
+							dlog("accept new connection ");
+							this->init_conn(worker, socket);
+							do_accept();
+							}
+							else
+							{
+							elog("accept error");
+							}
+							});
+				}
+
+				WorkerPtr get_worker()
+				{
+					if (!m.user_workers.empty())
+					{
+						dlog("dispatch to worker {}", m.worker_index);
+						return m.user_workers[m.worker_index++ % m.user_workers.size()];
 					}
 					else
 					{
-						elog("accept error");
+						//dlog("dispatch work to listen worker {}", std::this_thread::get_id());
+						return m.listen_worker;
 					}
-				});
-			}
-
-			WorkerPtr get_worker()
-			{
-				if (!m.user_workers.empty())
-				{
-					dlog("dispatch to worker {}", m.worker_index);
-					return m.user_workers[m.worker_index++ % m.user_workers.size()];
 				}
-				else
+
+				void init_conn(WorkerPtr worker, SocketPtr socket)
 				{
-					//dlog("dispatch work to listen worker {}", std::this_thread::get_id());
-					return m.listen_worker;
+					if (worker)
+					{
+						asio::dispatch(worker->context(), [=]() {
+								auto conn = create_connection(socket, worker);				 
+								socket->init_read();
+								});
+					}
 				}
-			}
 
-			void init_conn(WorkerPtr worker, SocketPtr socket)
-			{
-				if (worker)
+				TPtr create_connection(SocketPtr sock, WorkerPtr worker)
 				{
-					asio::dispatch(worker->context(), [=]() {
-						auto conn = create_connection(socket, worker);				 
-						socket->init_read();
-					});
+					auto conn = m.factory->create();
+					conn->init(sock, worker, this);
+					return conn;
 				}
-			}
 
-			TPtr create_connection(SocketPtr sock, WorkerPtr worker)
-			{
-				auto conn = m.factory->create();
-				conn->init(sock, worker, this);
-				return conn;
-			}
+				struct
+				{
+					uint32_t worker_index = 0;
+					std::vector<WorkerPtr> user_workers;
+					NetOptions options;
+					bool is_running = false;
+					void *ssl_context = nullptr;
+					FactoryPtr factory = nullptr;
+					std::vector<KNetHandler<T> *> event_handler_chain;
+					WorkerPtr listen_worker;
+				} m;
 
-			struct
-			{
-				uint32_t worker_index = 0;
-				std::vector<WorkerPtr> user_workers;
-				NetOptions options;
-				bool is_running = false;
-				void *ssl_context = nullptr;
-				FactoryPtr factory = nullptr;
-				std::vector<KNetHandler<T> *> event_handler_chain;
-				WorkerPtr listen_worker;
-			} m;
-
-			std::shared_ptr<asio::ip::tcp::acceptor> tcp_acceptor;
+				std::shared_ptr<asio::ip::tcp::acceptor> tcp_acceptor;
 		};
 
 		template <typename T>
-		using DefaultTcpListener = TcpListener<T, KNetFactory<T>, KNetWorker>;
+			using DefaultTcpListener = TcpListener<T, KNetFactory<T>, KNetWorker>;
 
 	} // namespace tcp
 
