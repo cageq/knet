dfe074a324c6d89bcc5f4c2fd7ddbde6b936d158
arthur <fatih@qq.com>
2022-03-03 17:40:37 +0800

    url parser error

--- include/utils/knet_url.hpp
+++ include/utils/knet_url.hpp
@@ -1,12 +1,18 @@
 
+
 #pragma once 
 #include <string> 
 #include <vector>
 #include <unordered_map>
+#include <sstream>
+#include <type_traits> 
+#include <algorithm> 
 
 
 namespace knet {
 namespace utils {
+
+
 /*
  
 tcp://127.0.0.1:3333?key=value&
@@ -23,13 +29,79 @@ options:
     bind_addr
     bind_port 
     multi  //multi address 
+    rbuf_size
+    sbuf_size 
+    encryption
+    auth_token
+    async
+    backlogs 
+    delay
+    threads 
  
 */
+
+    template<class T> 
+    struct TypeCaster{
+        T operator()(const std::string & val) {
+            T rst; 
+            std::stringstream ss ( val ); 
+            ss  >> rst; 
+            char c; 
+            //https://stackoverflow.com/questions/194465/how-to-parse-a-string-to-an-int-in-c 
+            if (ss.fail() || ss.get(c)) {
+                return T(); 
+            }
+            return rst; 
+        }
+    };
+
+    template<> 
+    struct TypeCaster<bool> {
+        bool operator()(const std::string &val ) {
+            std::string str = val ;
+            std::transform(str.begin(), str.end(),str.begin(), 
+                    [](unsigned char c){ return std::tolower(c);}
+                    );
+            if (val == "false" || val == "0" || val == "no"){
+                return false; 
+            }
+            return true; 
+        }
+    };
+
+    template<> 
+    struct TypeCaster<float> {
+        inline float operator()(const std::string &val ) {
+            return std::stof(val); 
+        }
+    };
+
+    template<> 
+    struct TypeCaster<double> {
+        inline double operator()(const std::string &val ) {
+            return std::stod(val); 
+        }
+    };
+
+    template<> 
+    struct TypeCaster<long double> {
+        inline long double operator()(const std::string &val ) {
+            return std::stold(val); 
+        }
+    };
+
+    template<> 
+    struct TypeCaster<std::string> {
+        inline std::string operator()(const std::string &val ) {
+            return val; 
+        }
+    };
+
+
 struct KNetUrl{
     std::string protocol;
     std::string host; 
     uint16_t    port; 
-    //std::vector<std::pair<std::string, std::string > > params;
     KNetUrl(const std::string & url = ""){
         encode(url); 
     }
@@ -40,8 +112,7 @@ struct KNetUrl{
         this->port = port; 
     }
 
-    std::unordered_map<std::string, std::string> params; 
- 
+
     bool encode(const std::string &channel)
     { 
         auto pos = channel.find("://");
@@ -52,15 +123,15 @@ struct KNetUrl{
         }
         pos += 2; //skip "//" after ":"
         
-        auto pPos = channel.find("?",pos); 
-        std::string hostInfo = channel.substr(pos +1 , pPos); 
+        auto pPos = channel.find('?',pos); 
+        std::string hostInfo = std::string (channel.begin() + pos +1 , channel.begin() + pPos); 
         auto cPos = hostInfo.find(":"); 
         if (cPos != std::string::npos){
             host = hostInfo.substr(0,cPos); 
-            auto port = hostInfo.substr(cPos +1); 
-            if (!port.empty())
+            auto p = hostInfo.substr(cPos +1); 
+            if (!p.empty())
             {
-                port = std::stoi( port); 
+                port = (uint16_t)std::stoi( p); 
             }
             
         }else {
@@ -103,13 +174,11 @@ struct KNetUrl{
         return url; 
     }
 
-    bool has(const std::string & key) const {
+    inline bool has(const std::string & key) const {
         return params.find(key) != params.end(); 
     }
  
- 
-    
-    void set(const std::string & key  , const std::string & val){
+    void set(const std::string & key, const std::string & val){
         params[key] = val; 
     }
 
@@ -119,6 +188,22 @@ struct KNetUrl{
             return itr->second; 
         }
         return dft; 
+    }
+    template <class T> 
+        T get(const std::string & key, T dft = T()) const  {
+            auto itr = params.find(key); 
+            if (itr!= params.end()){
+                return TypeCaster<T>{}(itr->second); 
+            }
+            return dft; 
+        }
+
+    std::string get_host() const {
+        return host; 
+    }
+
+    uint16_t get_port() const {
+        return port; 
     }
 
     std::string  dump() const {
@@ -130,8 +215,13 @@ struct KNetUrl{
         }
         return dumpStr; 
     }
+
+    private: 
+    std::unordered_map<std::string, std::string> params; 
 };
 
 
 } //namespace utils 
 } //namespace knet 
+
+
