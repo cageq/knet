274763072374482b85589b052e1598d5d6e335f4
arthur <fatih@qq.com>
2023-08-15 09:24:53 +0800

    update build

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 
 cd opt 
-tar zxf spdlog-1.11.0.tar.gz
-tar jxf asio-1.24.0.tar.bz2
-unzip -o fmt-9.1.0.zip
+#tar zxf spdlog-1.11.0.tar.gz
+#tar jxf asio-1.24.0.tar.bz2
+#unzip -o fmt-9.1.0.zip
 tar zxvf llhttp-v6.0.5.tgz
 
 cd ..
